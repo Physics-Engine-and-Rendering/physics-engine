0acf49d4506cc25bfe0df3e62f1054a5cf22e068
lukasino1214 <lukasino1214@gmail.com>
2023-09-20 20:53:42 +0200

    adding inline and moving implemntation to detail namespace

--- Math/math.hpp
+++ Math/math.hpp
@@ -22,145 +22,151 @@ namespace Math {
     using f32 = float;
     using f64 = double;
 
-    /* vectors */
-    template <typename T, usize N>
-    struct GenericVector {
-        std::array<T, N> array;
-        constexpr T &operator[](usize i) noexcept {return array[i];}
-        constexpr T const &operator[](usize i) const noexcept {return array[i];}
-        auto operator==(GenericVector<T, N> const &other) const -> bool = default;
-        auto operator!=(GenericVector<T, N> const &other) const -> bool = default;
-    };
+    namespace detail {
+        /* vectors */
+        template <typename T, usize N>
+        struct GenericVector {
+            std::array<T, N> array;
 
+            constexpr inline T &operator[](usize i) noexcept {return array[i];}
+            constexpr inline T const &operator[](usize i) const noexcept {return array[i];}
 
-    template <typename T>
-    struct GenericVector<T, 2> {
-        T x;
-        T y;
-        constexpr T & operator[](usize i) noexcept {
-            switch (i) {
-                case 1: return y;
-                default: return x;
+            auto operator==(GenericVector<T, N> const &other) const -> bool = default;
+            auto operator!=(GenericVector<T, N> const &other) const -> bool = default;
+        };
+
+        template <typename T>
+        struct GenericVector<T, 2> {
+            T x, y;
+            constexpr inline T & operator[](usize i) noexcept {
+                switch (i) {
+                    case 1: return y;
+                    default: return x;
+                }
             }
-        }
-        constexpr T const & operator[](usize i) const noexcept {
-            switch (i) {
-                case 1: return y;
-                default: return x;
+
+            constexpr inline T const & operator[](usize i) const noexcept {
+                switch (i) {
+                    case 1: return y;
+                    default: return x;
+                }
             }
-        }
-        auto operator==(GenericVector<T,2> const & other) const -> bool = default;
-        auto operator!=(GenericVector<T,2> const & other) const -> bool = default;
-    };
 
+            auto operator==(GenericVector<T,2> const & other) const -> bool = default;
+            auto operator!=(GenericVector<T,2> const & other) const -> bool = default;
+        };
 
-    template <typename T>
-    struct GenericVector<T, 3> {
-        T x, y, z;
-        constexpr T & operator[](usize i) noexcept {
-            switch (i) {
-                case 1: return y;
-                case 2: return z;
-                default: return x;
+        template <typename T>
+        struct GenericVector<T, 3> {
+            T x, y, z;
+            constexpr inline T & operator[](usize i) noexcept {
+                switch (i) {
+                    case 1: return y;
+                    case 2: return z;
+                    default: return x;
+                }
             }
-        }
-        constexpr T const & operator[](usize i) const noexcept {
-            switch (i) {
-                case 1: return y;
-                case 2: return z;
-                default: return x;
+
+            constexpr inline T const & operator[](usize i) const noexcept {
+                switch (i) {
+                    case 1: return y;
+                    case 2: return z;
+                    default: return x;
+                }
             }
-        }
-        auto operator==(GenericVector<T,3> const & other) const -> bool = default;
-        auto operator!=(GenericVector<T,3> const & other) const -> bool = default;
-    };
 
+            auto operator==(GenericVector<T,3> const & other) const -> bool = default;
+            auto operator!=(GenericVector<T,3> const & other) const -> bool = default;
+        };
 
-    template <typename T>
-    struct GenericVector<T, 4> {
-        T x, y, z, w;
-        constexpr T & operator[](usize i) noexcept {
-            switch (i) {
-                case 1: return y;
-                case 2: return z;
-                case 3: return w;
-                default: return x;
+        template <typename T>
+        struct GenericVector<T, 4> {
+            T x, y, z, w;
+            constexpr inline T & operator[](usize i) noexcept {
+                switch (i) {
+                    case 1: return y;
+                    case 2: return z;
+                    case 3: return w;
+                    default: return x;
+                }
             }
-        }
-        constexpr T const & operator[](usize i) const noexcept {
-            switch (i) {
-                case 1: return y;
-                case 2: return z;
-                case 3: return w;
-                default: return x;
+
+            constexpr inline T const & operator[](usize i) const noexcept {
+                switch (i) {
+                    case 1: return y;
+                    case 2: return z;
+                    case 3: return w;
+                    default: return x;
+                }
             }
-        }
-        auto operator==(GenericVector<T,4> const & other) const -> bool = default;
-        auto operator!=(GenericVector<T,4> const & other) const -> bool = default;
-    };
+
+            auto operator==(GenericVector<T,4> const & other) const -> bool = default;
+            auto operator!=(GenericVector<T,4> const & other) const -> bool = default;
+        };
 
 
-    /* matrices */
-    template <typename T, usize M, usize N>
-    struct GenericMatrix : GenericVector<GenericVector<T, N>, M> {};
+        /* matrices */
+        template <typename T, usize M, usize N>
+        struct GenericMatrix : GenericVector<GenericVector<T, N>, M> {};
+    }
 
     /* renaming */
-    using b32vec2 = GenericVector<b32, 2>;
-    using b32vec3 = GenericVector<b32, 3>;
-    using b32vec4 = GenericVector<b32, 4>;
-    using f32vec2 = GenericVector<f32, 2>;
-    using f32mat2x2 = GenericMatrix<f32, 2, 2>;
-    using f32mat2x3 = GenericMatrix<f32, 2, 3>;
-    using f32mat2x4 = GenericMatrix<f32, 2, 4>;
-    using f64vec2 = GenericVector<f64, 2>;
-    using f64mat2x2 = GenericMatrix<f64, 2, 2>;
-    using f64mat2x3 = GenericMatrix<f64, 2, 3>;
-    using f64mat2x4 = GenericMatrix<f64, 2, 4>;
-    using f32vec3 = GenericVector<f32, 3>;
-    using f32mat3x2 = GenericMatrix<f32, 3, 2>;
-    using f32mat3x3 = GenericMatrix<f32, 3, 3>;
-    using f32mat3x4 = GenericMatrix<f32, 3, 4>;
-    using f64vec3 = GenericVector<f64, 3>;
-    using f64mat3x2 = GenericMatrix<f64, 3, 2>;
-    using f64mat3x3 = GenericMatrix<f64, 3, 3>;
-    using f64mat3x4 = GenericMatrix<f64, 3, 4>;
-    using f32vec4 = GenericVector<f32, 4>;
-    using f32mat4x2 = GenericMatrix<f32, 4, 2>;
-    using f32mat4x3 = GenericMatrix<f32, 4, 3>;
-    using f32mat4x4 = GenericMatrix<f32, 4, 4>;
-    using f64vec4 = GenericVector<f64, 4>;
-    using f64mat4x2 = GenericMatrix<f64, 4, 2>;
-    using f64mat4x3 = GenericMatrix<f64, 4, 3>;
-    using f64mat4x4 = GenericMatrix<f64, 4, 4>;
-    using i32vec2 = GenericVector<i32, 2>;
-    using i32mat2x2 = GenericMatrix<i32, 2, 2>;
-    using i32mat2x3 = GenericMatrix<i32, 2, 3>;
-    using i32mat2x4 = GenericMatrix<i32, 2, 4>;
-    using u32vec2 = GenericVector<u32, 2>;
-    using u32mat2x2 = GenericMatrix<u32, 2, 2>;
-    using u32mat2x3 = GenericMatrix<u32, 2, 3>;
-    using u32mat2x4 = GenericMatrix<u32, 2, 4>;
-    using i32vec3 = GenericVector<i32, 3>;
-    using i32mat3x2 = GenericMatrix<i32, 3, 2>;
-    using i32mat3x3 = GenericMatrix<i32, 3, 3>;
-    using i32mat3x4 = GenericMatrix<i32, 3, 4>;
-    using u32vec3 = GenericVector<u32, 3>;
-    using u32mat3x2 = GenericMatrix<u32, 3, 2>;
-    using u32mat3x3 = GenericMatrix<u32, 3, 3>;
-    using u32mat3x4 = GenericMatrix<u32, 3, 4>;
-    using i32vec4 = GenericVector<i32, 4>;
-    using i32mat4x2 = GenericMatrix<i32, 4, 2>;
-    using i32mat4x3 = GenericMatrix<i32, 4, 3>;
-    using i32mat4x4 = GenericMatrix<i32, 4, 4>;
-    using u32vec4 = GenericVector<u32, 4>;
-    using u32mat4x2 = GenericMatrix<u32, 4, 2>;
-    using u32mat4x3 = GenericMatrix<u32, 4, 3>;
-    using u32mat4x4 = GenericMatrix<u32, 4, 4>;
+    using b32vec2 = detail::GenericVector<b32, 2>;
+    using b32vec3 = detail::GenericVector<b32, 3>;
+    using b32vec4 = detail::GenericVector<b32, 4>;
+    using f32vec2 = detail::GenericVector<f32, 2>;
+    using f32mat2x2 = detail::GenericMatrix<f32, 2, 2>;
+    using f32mat2x3 = detail::GenericMatrix<f32, 2, 3>;
+    using f32mat2x4 = detail::GenericMatrix<f32, 2, 4>;
+    using f64vec2 = detail::GenericVector<f64, 2>;
+    using f64mat2x2 = detail::GenericMatrix<f64, 2, 2>;
+    using f64mat2x3 = detail::GenericMatrix<f64, 2, 3>;
+    using f64mat2x4 = detail::GenericMatrix<f64, 2, 4>;
+    using f32vec3 = detail::GenericVector<f32, 3>;
+    using f32mat3x2 = detail::GenericMatrix<f32, 3, 2>;
+    using f32mat3x3 = detail::GenericMatrix<f32, 3, 3>;
+    using f32mat3x4 = detail::GenericMatrix<f32, 3, 4>;
+    using f64vec3 = detail::GenericVector<f64, 3>;
+    using f64mat3x2 = detail::GenericMatrix<f64, 3, 2>;
+    using f64mat3x3 = detail::GenericMatrix<f64, 3, 3>;
+    using f64mat3x4 = detail::GenericMatrix<f64, 3, 4>;
+    using f32vec4 = detail::GenericVector<f32, 4>;
+    using f32mat4x2 = detail::GenericMatrix<f32, 4, 2>;
+    using f32mat4x3 = detail::GenericMatrix<f32, 4, 3>;
+    using f32mat4x4 = detail::GenericMatrix<f32, 4, 4>;
+    using f64vec4 = detail::GenericVector<f64, 4>;
+    using f64mat4x2 = detail::GenericMatrix<f64, 4, 2>;
+    using f64mat4x3 = detail::GenericMatrix<f64, 4, 3>;
+    using f64mat4x4 = detail::GenericMatrix<f64, 4, 4>;
+    using i32vec2 = detail::GenericVector<i32, 2>;
+    using i32mat2x2 = detail::GenericMatrix<i32, 2, 2>;
+    using i32mat2x3 = detail::GenericMatrix<i32, 2, 3>;
+    using i32mat2x4 = detail::GenericMatrix<i32, 2, 4>;
+    using u32vec2 = detail::GenericVector<u32, 2>;
+    using u32mat2x2 = detail::GenericMatrix<u32, 2, 2>;
+    using u32mat2x3 = detail::GenericMatrix<u32, 2, 3>;
+    using u32mat2x4 = detail::GenericMatrix<u32, 2, 4>;
+    using i32vec3 = detail::GenericVector<i32, 3>;
+    using i32mat3x2 = detail::GenericMatrix<i32, 3, 2>;
+    using i32mat3x3 = detail::GenericMatrix<i32, 3, 3>;
+    using i32mat3x4 = detail::GenericMatrix<i32, 3, 4>;
+    using u32vec3 = detail::GenericVector<u32, 3>;
+    using u32mat3x2 = detail::GenericMatrix<u32, 3, 2>;
+    using u32mat3x3 = detail::GenericMatrix<u32, 3, 3>;
+    using u32mat3x4 = detail::GenericMatrix<u32, 3, 4>;
+    using i32vec4 = detail::GenericVector<i32, 4>;
+    using i32mat4x2 = detail::GenericMatrix<i32, 4, 2>;
+    using i32mat4x3 = detail::GenericMatrix<i32, 4, 3>;
+    using i32mat4x4 = detail::GenericMatrix<i32, 4, 4>;
+    using u32vec4 = detail::GenericVector<u32, 4>;
+    using u32mat4x2 = detail::GenericMatrix<u32, 4, 2>;
+    using u32mat4x3 = detail::GenericMatrix<u32, 4, 3>;
+    using u32mat4x4 = detail::GenericMatrix<u32, 4, 4>;
 
     /* math operations */
     template <typename T, usize N>
-    constexpr auto operator+(GenericVector<T, N> const &a, GenericVector<T, N> const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator+(detail::GenericVector<T, N> const &a, detail::GenericVector<T, N> const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] + b[i];
         }
@@ -168,8 +174,8 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto operator-(GenericVector<T, N> const &a, GenericVector<T, N> const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator-(detail::GenericVector<T, N> const &a, detail::GenericVector<T, N> const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] - b[i];
         }
@@ -177,8 +183,8 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto operator*(GenericVector<T, N> const &a, GenericVector<T, N> const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator*(detail::GenericVector<T, N> const &a, detail::GenericVector<T, N> const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] * b[i];
         }
@@ -186,8 +192,8 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto operator/(GenericVector<T, N> const &a, GenericVector<T, N> const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator/(detail::GenericVector<T, N> const &a, detail::GenericVector<T, N> const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] / b[i];
         }
@@ -195,17 +201,8 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto dot(GenericVector<T, N> const &a, GenericVector<T, N> const &b) -> T {
-        T result;
-        for (usize i = 0; i < N; i++) {
-            result += a[i] * b[i];
-        }
-        return result;
-    }
-
-    template <typename T, usize N>
-    constexpr auto operator+(GenericVector<T, N> const &a, T const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator+(detail::GenericVector<T, N> const &a, T const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] + b;
         }
@@ -213,8 +210,8 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto operator-(GenericVector<T, N> const &a, T const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator-(detail::GenericVector<T, N> const &a, T const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] - b;
         }
@@ -222,8 +219,8 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto operator*(GenericVector<T, N> const &a, T const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator*(detail::GenericVector<T, N> const &a, T const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] * b;
         }
@@ -231,10 +228,58 @@ namespace Math {
     }
 
     template <typename T, usize N>
-    constexpr auto operator/(GenericVector<T, N> const &a, T const &b) -> GenericVector<T, N> {
-        GenericVector<T, N> result;
+    constexpr inline auto operator/(detail::GenericVector<T, N> const &a, T const &b) -> detail::GenericVector<T, N> {
+        detail::GenericVector<T, N> result = {};
         for (usize i = 0; i < N; i++) {
             result[i] = a[i] / b;
+        }
+        return result;
+    }
+
+    template <typename T, usize M, usize N, usize P>
+    constexpr inline auto operator*(detail::GenericMatrix<T, M, N> const & a, detail::GenericMatrix<T, N, P> const & b) {
+        auto c = detail::GenericMatrix<T, M, P>{};
+        for (usize i = 0; i < M; ++i) {
+            for (usize j = 0; j < P; ++j) {
+                c[i][j] = 0;
+                for (usize k = 0; k < N; ++k) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        return c;
+    }
+
+    template <typename T, usize M, usize N>
+    constexpr inline auto operator*(detail::GenericMatrix<T, M, N> const & a, detail::GenericVector<T, N> const & v) {
+        auto c = detail::GenericVector<T, N>{};
+        for (usize i = 0; i < M; ++i) {
+            c[i] = 0;
+            for (usize k = 0; k < N; ++k) {
+                c[i] += a[i][k] * v[k];
+            }
+        }
+        return c;
+    }
+
+    template <typename T, usize N, usize P>
+    constexpr inline auto operator*(detail::GenericVector<T, N> const & v, detail::GenericMatrix<T, N, P> const & b) {
+        auto c = detail::GenericVector<std::remove_cv_t<T>, N>{};
+        for (usize j = 0; j < P; ++j) {
+            c[j] = 0;
+            for (usize k = 0; k < N; ++k) {
+                c[j] += v[k] * b[k][j];
+            }
+        }
+        return c;
+    }
+
+    /* mathematic functions */
+    template <typename T, usize N>
+    constexpr inline auto dot(detail::GenericVector<T, N> const &a, detail::GenericVector<T, N> const &b) -> T {
+        T result;
+        for (usize i = 0; i < N; i++) {
+            result += a[i] * b[i];
         }
         return result;
     }
